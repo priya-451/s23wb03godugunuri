08823767e3f127ff8f6329370930cc4f2947f4f2
priya-451 <S560603@nwmissouri.edu>
2023-04-05 19:30:32 -0500

    added all

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var CustomerRouter = require('./routes/Customer');
+var SelectorRouter = require('./routes/Selector');
+var boardRouter = require('./routes/board');
+
 
 var app = express();
 
@@ -21,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Customer', CustomerRouter);
+app.use('/Selector', SelectorRouter);
+app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
